b7628fdbbf8fa9100329fccc6daac4ec84bb8cf8
4IPE <all0b0y@yandex.ru>
2023-08-09 20:19:56 +0300

    <feat>Была добавлена новая функция для получения просмотра истории запроса

---
+++ src/ru/practicum/task_tracker/manager/InMemoryHistoryManager.java
@@ -0,0 +1,27 @@
+package ru.practicum.task_tracker.manager;
+
+import ru.practicum.task_tracker.tasks.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryHistoryManager implements HistoryManager{
+    private final List<Task> history = new ArrayList<>();
+    private static final int LIMIT = 10;
+
+    @Override
+    public void addTask(Task task) {
+        if(task==null){
+            return;
+        }
+        history.add(task);
+        if(history.size()>10){
+            history.remove(0);
+        }
+    }
+
+    @Override
+    public List<Task> getHistory() {
+        return new ArrayList<>(history);
+    }
+}
